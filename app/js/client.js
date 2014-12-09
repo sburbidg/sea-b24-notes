853109ac8b4cd8801ead815768a3e048ee60ba5a
toastynerd <tylermorgan86@gmail.com>
2014-12-08 16:45:28 -0800

    finally fixed this fucking directive

--- app/js/client.js
+++ app/js/client.js
@@ -3,6 +3,10 @@ require('angular-route');
 
 var notesApp = angular.module('notesApp', ['ngRoute']);
 
+//directives
+require('./directives/dummy_direc')(notesApp);
+require('./notes/directives/new_note_form_direc')(notesApp);
+
 //services
 require('./services/resource_backend_service')(notesApp);
 
